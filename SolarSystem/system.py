8f78b8b85ed6a44016b0b60589ca7f41687a6953
ultrongr <80486057+ultrongr@users.noreply.github.com>
2023-07-19 21:43:44 +0300

    Improved zoom and locking

--- SolarSystem/system.py
+++ SolarSystem/system.py
@@ -10,8 +10,7 @@ import time
 
 # screen_size  = [2700, 1050]
 screen_size  = [1500, 880]
-zoom = 1
-
+zoom = 2
 class Planet:
 
     def __init__(self, canvas, x, y, velocity, radius, mass, color, personal_display_size, immovable=False):
@@ -25,6 +24,7 @@ class Planet:
         self.color = color
         self.immovable = immovable
         self.planet = None
+        self.name = None
         self.draw()
     
     def draw(self):
@@ -48,6 +48,8 @@ class Win:
         self.previous_drag = (0,0)
         self.planets = []
         self.time_step=10**2.5
+        self.locked = False
+        self.locked_on = 0
         self.populate()
         self.time=0
         self.simulate()
@@ -58,20 +60,34 @@ class Win:
         #radius in 1000km
         #mass in kg
 
-        # self.planets.append(Planet(self.canvas, x=10, y=10, velocity=(0,0), radius=696.340, mass=1.989*10**30 , color="yellow", personal_display_size=10**-2))
-        # self.planets.append(Planet(self.canvas, x=40, y=40, velocity=(0,0), radius=6.371, mass=5.972*10**24 , color="blue", personal_display_size=10**-1))
-        # self.planets.append(Planet(self.canvas, x=42, y=42, velocity=(0,0), radius=1.736, mass=7.346*10**22 , color="gray", personal_display_size=10**-1))
-        
-        # self.planets.append(Planet(self.canvas, x=40, y=40, velocity=(0,0), radius=6.371, mass=5.972*10**24 , color="blue", personal_display_size=10**-1,immovable=True))
-        # self.planets.append(Planet(self.canvas, x=40, y=50, velocity=(0.2,0), radius=6.371, mass=5.972*10**24 , color="blue", personal_display_size=10**-1))
-        # self.planets.append(Planet(self.canvas, x=40, y=60, velocity=(-0.1,0), radius=6.371, mass=5.972*10**24 , color="blue", personal_display_size=10**-1))
-        spawn_offset =[180, 180]
-        self.planets.append(Planet(self.canvas, x=0+spawn_offset[0], y=0+spawn_offset[1], velocity=(0,0), radius=696.340, mass=1.989*10**30 , color="yellow", personal_display_size=10**-2, immovable=True))
-        self.planets.append(Planet(self.canvas, x=147+spawn_offset[0], y=0+spawn_offset[1], velocity=(0,30.3), radius=6.371, mass=5.972*10**24 , color="blue", personal_display_size=10**-0.5,immovable=False))
-        self.planets.append(Planet(self.canvas, x=147.360+spawn_offset[0], y=0+spawn_offset[1], velocity=(0,31.082), radius=1.737, mass=7.3*10**22 , color="grey", personal_display_size=10**-0.5,immovable=False))
 
+        spawn_offset =[180, 180]
+        sun = Planet(self.canvas, x=0+spawn_offset[0], y=0+spawn_offset[1], velocity=(0,0), radius=696.340, mass=1.989*10**30 , color="yellow", personal_display_size=10**-2, immovable=True)
+        earth = Planet(self.canvas, x=147+spawn_offset[0], y=0+spawn_offset[1], velocity=(0,30.3), radius=6.371, mass=5.972*10**24 , color="blue", personal_display_size=10**-2,immovable=False)
+        moon = Planet(self.canvas, x=147.360+spawn_offset[0], y=0+spawn_offset[1], velocity=(0,30.3+1.082), radius=1.737, mass=7.3*10**22 , color="grey", personal_display_size=10**-2,immovable=False)
+
+        self.planets.append(sun)
+        self.planets.append(earth)
+        self.planets.append(moon)
+        sun.name = "Sun"
+        earth.name = "Earth"
+        moon.name = "Moon"
+
+        self.locked=False
+        self.locked_on=0
     
     def simulate(self):
+
+        if self.locked:
+            locked_cords = [self.planets[self.locked_on].x, self.planets[self.locked_on].y]
+            screen_center = [screen_size[0]/2, screen_size[1]/2]
+            offset = [screen_center[0]-locked_cords[0]*zoom, screen_center[1]-locked_cords[1]*zoom]
+
+            for planet in self.planets:
+                planet.x += offset[0]/zoom
+                planet.y += offset[1]/zoom
+                planet.draw()
+        
 
         """Calculate velocities"""
         for planet in self.planets:
@@ -84,31 +100,28 @@ class Win:
                     other_m = other_planet.mass # kg -> kg
                     K = 6.67408*10**-11
                     distance = (Dx**2 + Dy**2)**0.5
-                    # print(distance)
+                    # if distance < 140318173421:
+                    #     print(distance, planet.x, planet.y, zoom)
+
                     acceleration = (K * other_planet.mass / distance**2)/1000 # m/s^2 -> km/s^2
-                    # print(1000*acceleration)#*planet.mass)
+                    # if planet.name == "Moon" and other_planet.name == "Earth":
+                    #     print("Moon, earth:", acceleration)
+                    # if planet.name == "Moon" and other_planet.name == "Sun":
+                    #     print("Moon,   sun:", acceleration)
                     planet.velocity = (planet.velocity[0]-self.time_step*acceleration * Dx / distance, planet.velocity[1]-self.time_step* acceleration * Dy / distance)
-                    # print(planet.velocity)
+
         
 
         """Calculate positions"""
-        counter=0
         for planet in self.planets:
             if planet.immovable:
                 planet.draw()
                 continue
-            counter+=1
             # velocity is in km/s
             # planet.x is in M km
             planet.x += planet.velocity[0]*(self.time_step/10**6)
             planet.y += planet.velocity[1]*(self.time_step/10**6)
-            if counter == 1:
-                pass
-                # print(planet.velocity[0]*(self.time_step/10**6))
-                # print(planet.velocity[1]*(self.time_step/10**6))
-                # print(planet.x)
-                # print(planet.y)
-                # print("")
+
             planet.draw()
         self.time = self.time_step + self.time
         # print(self.time/(24*3600))
@@ -126,6 +139,8 @@ class Win:
         self.previous_drag = (event.x, event.y)
     
     def drag(self, event):
+        if self.locked:
+            return
         if self.previous_drag==(0,0):
             self.previous_drag = (event.x, event.y)        
         offset = [event.x-self.previous_drag[0], event.y-self.previous_drag[1]]
@@ -136,6 +151,8 @@ class Win:
             planet.draw()
     
     def keypress(self, event):
+        if self.locked:
+            return
         offset =[0,0]
         moving = { "w": [0, 1], "a": [1,0], "s": [0,-1], "d": [-1,0] }
         offset = moving.get(event.char, [0,0])
@@ -149,8 +166,23 @@ class Win:
 
     def wheel(self, event):
         global zoom
+        old_zoom = zoom
         zoom+=0.1*zoom*event.delta/120
-        # self.previous_drag=(0,0)
+
+        
+        if self.locked:
+            locked_cords = [self.planets[self.locked_on].x, self.planets[self.locked_on].y]
+            screen_center = [screen_size[0]/2, screen_size[1]/2]  
+            offset = [screen_center[0]-locked_cords[0]*zoom, screen_center[1]-locked_cords[1]*zoom]
+        else:
+            locked_cords= [event.x/old_zoom, event.y/old_zoom]
+            offset = [event.x-locked_cords[0]*zoom, event.y-locked_cords[1]*zoom]      
+
+        
+        for planet in self.planets:
+            planet.x += offset[0]/zoom
+            planet.y += offset[1]/zoom
+            planet.draw()
 
 root = tk.Tk()
 root.geometry(f"{screen_size[0]}x{screen_size[1]}+10+10")
